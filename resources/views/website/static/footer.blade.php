df8ef8ec21ff9759de85d3ff811bc87a5373eb89
DexterPHP <mohammad.samir.khalaf@gmail.com>
2020-09-28 08:45:06 +0300

    Upload First Version

--- resources/views/website/static/footer.blade.php
+++ resources/views/website/static/footer.blade.php
@@ -2,12 +2,11 @@
 <footer class="pt-lg-5 inner-page-footer">
     <div class="social_media_w3layouts text-center">
         <ul class="social-icons justify-content-center mt-md-5 mt-3">
-            <li class="mr-1"><a href="#"><span class="fab fa-facebook-f"></span></a></li>
-            <li class="mx-1"><a href="#"><span class="fab fa-twitter"></span></a></li>
-            <li class="mx-1"><a href="#"><span class="fab fa-google-plus-g"></span></a></li>
-            <li class="mx-1"><a href="#"><span class="fab fa-linkedin-in"></span></a></li>
+            <li class="mr-1"><a href="https://fb.com/fenix.p2h"><span class="fab fa-facebook-f"></span></a></li>
+            <li class="mx-1"><a href="https://twitter.com/#><span class="fab fa-twitter"></span></a></li>
+
         </ul>
-        <p class="copy-right-w3ls my-3">© 2018 PhotoWall. All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+        <p class="copy-right-w3ls my-3">© 2020 PHP Test. All rights reserved | D&P by <a href="https://fb.com/fenix.p2h">Muhammad Khalaf</a></p>
     </div>
 </footer>
 
@@ -44,13 +43,13 @@
 <script>
     $(document).ready(function() {
         /*
-                                var defaults = {
-                                      containerID: 'toTop', // fading element id
-                                    containerHoverID: 'toTopHover', // fading element hover id
-                                    scrollSpeed: 1200,
-                                    easingType: 'linear'
-                                 };
-                                */
+        var defaults = {
+              containerID: 'toTop', // fading element id
+            containerHoverID: 'toTopHover', // fading element hover id
+            scrollSpeed: 1200,
+            easingType: 'linear'
+         };
+        */
 
         $().UItoTop({
             easingType: 'easeOutQuart'
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', 'HomeController@index');
+Route::get('/dashboard', function(){
+    return view('dashboard');
+});
 Route::get('contact', 'HomeController@contact');
 
 
